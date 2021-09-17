fe62bbdccface1ed15beccad8bb9fff89d8d4f06
Uzen Huang <uzen.huang@gmail.com>
2021-09-17 09:33:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ibbycal
+
+_An IB compatible, 8 (modifiable) day cycle calendar written in python._
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ibbynotes',
+    version='1.0',
+    description='An IB compatible, 8 (modifiable) day cycle calendar written in python.',
+    author='CookieUzen',
+    author_email='uzen.huang@gmail.com',
+    package=['ibbycal'],
+    install_requires=['ics'],
+)
