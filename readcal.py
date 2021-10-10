0925f8e2ae9300b982f06fd516967ff24e6799c8
Uzen Huang <uzen.huang@gmail.com>
2021-10-10 09:17:00 +0800

    parse yaml for classes and timetables

--- readcal.py
+++ readcal.py
@@ -2,7 +2,27 @@
 
 import yaml
 
-with open('config.yaml') as foo:
-    bar = yaml.safe_load(foo)
 
-print(bar)
+class classes:
+    def __init__(self, name, classroom, teacher):
+        self.name = name
+        self.classroom = classroom
+        self.teacher = teacher
+
+
+with open('config.yaml') as foo:
+    data = yaml.safe_load(foo)
+
+# Creating a dictionary of classes from config.yaml
+dictofclass = {}
+for i in range(len(data['classes'])):
+    dictofclass.update({data['classes'][i]['name']:
+                        classes(data['classes'][i]['name'],
+                        data['classes'][i]['classroom'],
+                        data['classes'][i]['teacher'])})
+
+# Create an array for timetable
+timetable = list()
+
+for i in range(len(data['timetable'])):
+    timetable.append(data['timetable'][i])
