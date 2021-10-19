210f9fa42414f7642bddd166f33e9913c846b28b
CookieUzen <uzen.huang@gmail.com>
2021-10-19 15:33:30 +0800

    generator update

--- generator.py
+++ generator.py
@@ -3,8 +3,6 @@
 import yaml
 
 # print("When are your workdays? [1 2 3-7]")
-# A = input()
-# Parse this
 
 def addClass():
     """add classess"""
@@ -19,11 +17,12 @@ def addClass():
         print("Enter teacher: ", end="")
         classTeacher = input()
 
-        classes.append({'name': className, 'room': classRoom, 'teacher': classTeacher})
+        classes.append({'name': className, 'classroom': classRoom, 'teacher': classTeacher})
 
         print("Add another class [Y/n]: ", end="")
         if input() in ('n', 'N', 'No', 'no'):
             break
+        print()
 
     print()
     return(classes)
@@ -32,16 +31,23 @@ def addClass():
 def addTimetable():
     """add timetable"""
     timetable = list()
-    while True:
-        print("Input classes, with a space seperating each class. Class name have to the ones entered above.")
-        timetable.append(input().split())
 
+    print("Input classes, with a comma seperating each class. Class name have to the ones entered above.")
+    while True:
+        read = input().split(",")
+        strippedRead = [ i.strip() for i in read ]
+
+        timetable.append(strippedRead)
         print("Add another day [Y/n]: ", end="")
         if input() in ('n', 'N', 'No', 'no'):
             break
+        print()
 
     print()
     return(timetable)
 
 
 output = {'classes': addClass(), 'timetable': addTimetable()}
+
+with open('config.yaml', 'w') as file:
+    yaml.dump(output, file)
